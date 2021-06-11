1edc580acb10ddf25a47e44996717e248f705590
lapseofjudgementcrew <lapseofjudgementcrew@gmail.com>
2021-06-11 03:38:34 +0000

    use vue components

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,8 +20,7 @@
     </head>
     <body class="antialiased">
         <div id="app">
-            <h1>@{{product}}</h1>
-            <p>@{{description}}</p>
+            
             <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center py-4 sm:pt-0">
                 <hello-world />
             </div>
